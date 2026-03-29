2e3a52a6bc206f293485bca8cf5ed714582e2fba
Taneeshhhh <taneeshsuresh3@gmail.com>
2026-03-29 15:03:10 +0530

    Removed duplicate src structure, standardized backend structure

--- backend/src/controllers/userController.js
+++
@@ -1,8 +0,0 @@
-const db = require("../config/db");
-
-exports.getUsers = (req, res) => {
-    db.query("SELECT * FROM users", (err, result) => {
-        if (err) return res.status(500).json(err);
-        res.json(result);
-    });
-};
